96b46870e2cc01b21077412f13cb20cad6651540
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-11-16 10:20:49 -0700

    Update turtletest.py

--- turtletest.py
+++ turtletest.py
@@ -26,24 +26,42 @@ t = turtle.Turtle()
 s.tracer(0, 0)
 
 
-mySys.iterate(5)
-
-for turnAngle in range(60,-1,-1):
-    #rest the turtle
-    t.goto(0,0)
-    t.clear()
-    t.setheading(90)
-    
-    #perform each draw action for a certain letter
-    for letter in mySys.sentence:
-        if letter in ("A","B"):
-            t.fd(4)
-        elif letter == "-":
-            t.right(turnAngle)
-        elif letter == "+":
-            t.left(turnAngle)
-    
-    #update screen
-    s.update()
+for iteration in range(0,6):
+    mySys.iterate(1)
+    for turnAngle in range(0,61,1):
+        #rest the turtle
+        t.goto(0,-250)
+        t.clear()
+        t.setheading(90)
+        
+        #perform each draw action for a certain letter
+        for letter in mySys.sentence:
+            if letter in ("A","B"):
+                t.fd(128/2**iteration)
+            elif letter == "-":
+                t.right(turnAngle)
+            elif letter == "+":
+                t.left(turnAngle)
+        
+        #update screen
+        s.update()
+        
+    for turnAngle in range(60,-1,-1):
+        #rest the turtle
+        t.goto(0,-250)
+        t.clear()
+        t.setheading(90)
+        
+        #perform each draw action for a certain letter
+        for letter in mySys.sentence:
+            if letter in ("A","B"):
+                t.fd(128/2**iteration)
+            elif letter == "-":
+                t.right(turnAngle)
+            elif letter == "+":
+                t.left(turnAngle)
+        
+        #update screen
+        s.update()
     
 turtle.done()
